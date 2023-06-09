b0c0e2aaabdbdd5d3d8003b8ab0fedf3f3df03a0
martomorri <106319796+martomorri@users.noreply.github.com>
2023-06-09 12:00:53 -0300

    terminado; timer roto

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import axios from 'axios';
 function App() {
   const [countries, setCountries] = useState([]);
   const [points, setPoints] = useState(0);
-  const [random, setRandom] = useState(Math.floor(Math.random() * 221));
+  const [random, setRandom] = useState(Math.floor(Math.random() * 220));
+  const [isLoading, setIsLoading] = useState(true);
+  // const [randomIndex, setRandomIndex] = useState([])
+  const [timer, setTimer] = useState(15);
 
   useEffect(() => {
     axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
@@ -13,16 +16,56 @@ function App() {
         console.log(res);
         setCountries(
           res.data.data.map(c => ({ name: c.name, flag: c.flag }))
-        )  
+        )
+        setIsLoading(false);
       })
+      // for (let index = 0; index < countries.length; index++) {
+      //   setRandomIndex([...randomIndex, index]);
+      // }
     }, []);
 
+    setInterval (() => {
+      if (timer > 0) setTimer(timer-1);
+      else {
+        setRandom(Math.floor(Math.random() * 220));
+        setTimer(15);
+      }
+    }, 1000);
 
-  // console.log(countries);
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(countries[random].name);
+      if(e.target.ans.value.toLowerCase() === countries[random].name.toLowerCase()) {
+        setPoints(points + 10);
+        //setRandomIndex(randomIndex.filter(r => random !== r));
+        setRandom(Math.floor(Math.random() * 220));
+      } else {
+        if (points > 0)  setPoints(points - 1);
+      }
+      e.target.ans.value = "";
+    }
+
+    const skipCountry = () => {
+      if (points > 0)  setPoints(points - 1);
+      //setRandomIndex(randomIndex.filter(r => random !== r));
+      setTimer(15);
+      setRandom(Math.floor(Math.random() * 220));
+    }
 
   return (
+    isLoading ? <center><p>loading...</p></center> :
     <div className="App">
-      
+      <div className='points'>
+        <p className='h1'>Time: {timer}</p>
+        <p className='h1'>Points: {points}</p>
+      </div>
+      <img className='flag-cont' src={countries[random].flag} alt='flag'/>
+      <br></br>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input className='form-control w-25' type="text" name="ans" placeholder='Country name...'></input>
+        <button className='btn btn-primary' type='submit'>Guess</button>
+        <button className='btn btn-secondary' onClick={skipCountry}>Skip →</button>
+      </form>
     </div>
   );
 }
