246b8e141a604898696a403f3a27225fac2e42c4
martomorri <106319796+martomorri@users.noreply.github.com>
2023-06-15 09:58:34 -0300

    js es el peor lenguaje inventado en la historia

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import axios from 'axios';
 function App() {
   const [countries, setCountries] = useState([]);
   const [points, setPoints] = useState(0);
-  const [random, setRandom] = useState(Math.floor(Math.random() * 220));
+  const [random, setRandom] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [timer, setTimer] = useState(15);
-  const [clues, setClues] = useState('');
+  const [clues, setClues] = useState([]);
   const [ref, setRef] = useState();
+  // const [clues, setClues] = useState();
 
   useEffect(() => {
     axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
@@ -18,9 +19,17 @@ function App() {
         setCountries(
           res.data.data.map(c => ({ name: c.name, flag: c.flag }))
         )
+        setRandom(Math.floor(Math.random() * 220));
         setIsLoading(false);
       })
   }, []);
+
+  useEffect(() => {
+    if(!isLoading) {
+      console.log(countries[random]);
+      cleanClues(countries[random].name);
+    }
+  }, [random]);
 
   useEffect(() => {
     if(ref) clearTimeout(ref);
@@ -29,7 +38,6 @@ function App() {
         if (timer > 0) setTimer(t => t-1);
         else {
           setRandom(Math.floor(Math.random() * 220));
-          cleanClues(countries[random].name);
           setTimer(15);
         }
       }, 1000)
@@ -38,10 +46,21 @@ function App() {
 
   const cleanClues = (n) => {
     console.log(n.length);
-    setClues('');
+    setClues([]);
     let nc = '';
-    for (let i = 0; i <= n.length; i++) {
+    for (let i = 0; i < n.length; i++) {
       console.log(i);
+      setClues([...clues, '_']);
+    }
+    setClues(nc);
+  }
+
+  const giveClue = () => {
+    let n = countries[random].name
+    let rc = Math.floor(Math.random() * n);
+    let nc = clues;
+    for (let i = 0; i < n.length; i++) {
+      if (i === rc) nc += n[i];
       nc += '_ ';
     }
     setClues(nc);
@@ -53,7 +72,6 @@ function App() {
     if (e.target.ans.value.toLowerCase() === countries[random].name.toLowerCase()) {
       setPoints(points + 10);
       setRandom(Math.floor(Math.random() * 220));
-      cleanClues(countries[random].name);
       setTimer(15);
     } else {
       if (points > 0) setPoints(points - 1);
@@ -65,7 +83,6 @@ function App() {
     if (points > 0) setPoints(points - 1);
     setTimer(15);
     setRandom(Math.floor(Math.random() * 220));
-    cleanClues(countries[random].name);
   }
 
   return (
@@ -76,9 +93,11 @@ function App() {
           <p className='h1'>Points: {points}</p>
         </div>
         <img className='flag-cont' src={countries[random].flag} alt='flag' />
-        <p className='fw-bold text-white'>{clues}</p>
+        {clues.map(c => <p className='h1 fw-bold text-white'>{c}</p>).join(" ")}
+        {/* <p className='h1 fw-bold text-white'>{clues}</p> */}
         <br></br>
         <form onSubmit={e => handleSubmit(e)}>
+          <button className='btn btn-success' type='button' onClick={giveClue}>Clue</button>
           <input className='form-control w-25' type="text" name="ans" placeholder='Country name...'></input>
           <button className='btn btn-primary' type='submit'>Guess</button>
           <button className='btn btn-secondary' type='button' onClick={skipCountry}>Skip →</button>
